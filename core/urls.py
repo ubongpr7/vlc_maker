6481fe8ca19fb9cd03607c0ad313ef52169c49d2
Ubong Prosper <ubongpr7@gmail.com>
2024-09-13 20:45:42 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('', include('mainapps.home.urls',namespace='home')),
     path('video/', include('mainapps.video.urls',namespace='video')),
     path('text/', include('mainapps.vidoe_text.urls',namespace='video_text')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
