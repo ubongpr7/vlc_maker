f10946fb20f16ef75274088b75f1a4ea8cf18689
ubongpr7 <ubongpr7@gmail.com>
2024-09-15 23:49:56 +0100

    gotten

--- core/wsgi.py
+++ core/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from static_ranges import Ranges
+from dj_static import Cling,MediaCling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Ranges(Cling(MediaCling(get_wsgi_application())))
