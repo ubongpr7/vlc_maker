1a90d25a3b8e7f49e74d0e7abc3abc27835593ec
ubongpr7 <ubongpr7@gmail.com>
2024-09-18 18:14:36 +0100

    loader

--- mainapps/accounts/views.py
+++ mainapps/accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 
 # Create your views here.
 def login(request):
+    
     if request.method == 'POST':
         # Get username and password from the form
         username = request.POST['username']
@@ -17,11 +18,16 @@ def login(request):
             # If the user exists, log them in
             auth_login(request, user)  # Note: Using `auth_login` here to avoid conflict
             messages.success(request, 'Successfully logged in!')
-            return redirect('/text')  # Redirect to a 'home' page or any other page after login
+            try:
+                return redirect(request.session.get('next'))  
+            except:
+                return redirect('/text')  
+
         else:
             # If login failed, send an error message
             messages.error(request, 'Invalid username or password. Please try again.')
-
+    next=request.GET.get('next','')
+    request.session['next']=next
     return render(request,'accounts/login.html',)
 
 
