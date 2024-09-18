cc99e29e555a0d7cea47fac6f24c9d254cf1d45b
ubongpr7 <ubongpr7@gmail.com>
2024-09-18 17:26:43 +0100

    Login

--- mainapps/accounts/views.py
+++ mainapps/accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
+
 
 # Create your views here.
 def login(request):
-    
-    if request.method == 'POST':
+        if request.method == 'POST':
         # Get username and password from the form
         username = request.POST['username']
         password = request.POST['password']
@@ -15,12 +15,13 @@ def login(request):
         
         if user is not None:
             # If the user exists, log them in
-            login(request, user)
+            auth_login(request, user)  # Note: Using `auth_login` here to avoid conflict
             messages.success(request, 'Successfully logged in!')
-            return redirect('/text')  # Redirect to a 'home' page or any other page after login
+            return redirect('home')  # Redirect to a 'home' page or any other page after login
         else:
             # If login failed, send an error message
             messages.error(request, 'Invalid username or password. Please try again.')
+
     return render(request,'accounts/login.html',)
 
 
