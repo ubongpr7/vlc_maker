91b5d0dfda75b7e85fd948b1f8d18ca6e046beb7
Ubong Prosper <ubongpr7@gmail.com>
2024-09-19 16:52:09 +0100

    stripe

--- mainapps/accounts/urls.py
+++ mainapps/accounts/urls.py
@@ -4,5 +4,5 @@ app_name='accounts'
 urlpatterns=[
     path('login/',login,name='signin'),
     path('embedded_pricing_page/',embedded_pricing_page,name='embedded_pricing_page'),
-    path('subscription_confirm/',subscription_confirm,name='subscription_confirm'),
+    path('confirm-subscription/',subscription_confirm,name='subscription_confirm'),
 ]
