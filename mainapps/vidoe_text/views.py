695a20c15885dc412f09faccf18a70cfb3242499
ubongpr7 <ubongpr7@gmail.com>
2024-09-21 14:51:15 +0100

    Processing and textClip

--- mainapps/vidoe_text/views.py
+++ mainapps/vidoe_text/views.py
@@ -209,93 +209,11 @@ def process_textfile(request, textfile_id):
 
     try:
         call_command('process_video', textfile_id)
-        return JsonResponse({'success': f'Processing initiated for text file {textfile_id}.'})
+        return redirect(reverse('video_text:progress', args=['build',textfile_id]))
+
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-# @login_required
-# def process_textfile(request, textfile_id):
-#     try:
-#         # Fetch the TextFile instance
-#         textfile = TextFile.objects.get(pk=textfile_id)
-#         if textfile.user != request.user:
-#             messages.error(request,'You Do Not have access to the Resources You Requested ')
-
-#             return render(request,'permission_denied.html')
-#     except TextFile.DoesNotExist:
-#         return Http404("Text file not found")
-
-#     # Check if the necessary fields are present in TextFile
-#     if not textfile.text_file:
-#         return JsonResponse({"error": "Text file is missing."}, status=400)
-
-#     # Prepare paths for video and output files
-#     base_path = settings.MEDIA_ROOT
-#     output_video_path = os.path.join(base_path, 'videos', f"{textfile_id}_output.mp4")
-
-#     # Gather all TextLineVideoClip instances for this TextFile
-#     video_clips = textfile.video_clips.all().order_by('line_number')
-#     if not video_clips.exists():
-#         return JsonResponse({"error": "No video clips found for this TextFile."}, status=400)
-
-#     # Create a mapping of line numbers to their respective video paths or default to a placeholder
-#     video_paths = []
-#     for clip in video_clips:
-#         if clip.video_file:
-#             video_path=clip.video_file
-#         else:
-#             video_path = clip.video_file_path if clip.video_file_path else ''  # Fallback to empty string if not available
-
-#         video_paths.append({
-#             "line_number": clip.line_number,
-#             "video_path": video_path,
-#             "timestamp_start": clip.timestamp_start,
-#             "timestamp_end": clip.timestamp_end
-#         })
-#     video_clips_dict = [clip.to_dict() for clip in video_clips]
-#     # video_clips_json = json.dumps(video_clips_dict)
-#     # Convert video_paths to a string for passing to the script
-#     video_paths_str = str(video_paths)
-
-#     json_file_path =f'{base_path}/tmp/{textfile_id}/video_clips.json'
-#     # Check if the output file already exists and delete it
-#     if os.path.exists(json_file_path):
-#         os.remove(json_file_path)
-
-#     # Create the necessary directories if they do not exist
-#     os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
-    
-#     # Write the serialized data to a JSON file
-#     with open(json_file_path, 'w') as f:
-#         json.dump(video_clips_dict, f)
-#         # f.write(str(video_clips_dict))
-#     resolution=textfile.resolution
-#     # Prepare the command to call the external script (e.g., MoviePy and Aeneas processing)
-#     command = [
-#         'python3.10', 'process_video.py',
-#         textfile.text_file.url,
-#         json_file_path,       # Pass video paths mapping
-#         output_video_path,     # Path for the output video
-#         textfile.voice_id,     # Pass the voice_id
-#         textfile.api_key  ,     # Pass the api_key
-#         base_path,
-#         textfile_id,
-#         resolution,
-#         textfile.font_color,
-#         textfile.subtitle_box_color,
-#         textfile.font_file.url,
-#         str(textfile.font_size),
-        
-#     ]
-
-#     # Start the subprocess
-#     try:
-#         subprocess.Popen(command,)  # Run it asynchronously
-#     except Exception as e:
-#         return JsonResponse({"error": f"Error running subprocess: {str(e)}"}, status=500)
-
-#     # Return success message
-#     return redirect(reverse('video_text:progress', args=['build',textfile_id]))
     
 
 @login_required
