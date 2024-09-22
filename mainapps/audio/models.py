ac574b8b4ae2f0057fd52dca0face887adf67362
ubongpr7 <ubongpr7@gmail.com>
2024-09-22 23:39:58 +0100

    water color cv2

--- mainapps/audio/models.py
+++ mainapps/audio/models.py
@@ -10,7 +10,7 @@ from mainapps.vidoe_text.models import TextFile
 class BackgroundMusic(models.Model):
     text_file=models.ForeignKey(TextFile, on_delete=models.CASCADE, related_name='background_musics')
     title = models.CharField(max_length=255,null=True,blank=True)
-    music_file = models.FileField(upload_to='bg_music_file_upload_path/')
+    music_file = models.FileField(upload_to='bg_music_file_upload_path')
     start_time = models.FloatField(help_text="Start time in seconds")
     end_time = models.FloatField(help_text="End time in seconds")
     # video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='background_music')
