370d438f373b22c0cd492311171cf32afad22a7c
ubongpr7 <ubongpr7@gmail.com>
2024-09-22 23:44:18 +0100

    water color cv2

--- mainapps/audio/models.py
+++ mainapps/audio/models.py
@@ -6,11 +6,16 @@ from django.db import models
 from mainapps.video.models import Video
 from mainapps.vidoe_text.models import TextFile
 
+def bg_music_file_upload_path(instance, filename):
+    """Generate a unique file path for each uploaded text file."""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}{instance.text_file.id}.{ext}'  # Use UUID to ensure unique file names
+    return os.path.join('background', filename)
 
 class BackgroundMusic(models.Model):
     text_file=models.ForeignKey(TextFile, on_delete=models.CASCADE, related_name='background_musics')
     title = models.CharField(max_length=255,null=True,blank=True)
-    music_file = models.FileField(upload_to='bg_music_file_upload_path')
+    music=models.FileField(null=True, blank=True, upload_to='bg_misics')
     start_time = models.FloatField(help_text="Start time in seconds")
     end_time = models.FloatField(help_text="End time in seconds")
     # video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='background_music')
