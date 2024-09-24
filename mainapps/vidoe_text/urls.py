552a7556854383a4183b699c50169f4b0bcfbfe5
ubongpr7 <ubongpr@gmail.com>
2024-09-24 21:45:32 +0100

    assets

--- mainapps/vidoe_text/urls.py
+++ mainapps/vidoe_text/urls.py
@@ -10,7 +10,7 @@ urlpatterns=[
     path('progress_page/<str:al_the_way>/<str:text_file_id>', progress_page, name='progress_page'),
     path('progress/<str:text_file_id>/', progress, name='progress'),
     path('process-background-music/<str:textfile_id>/', process_background_music, name='process_background_music'),path('media/<str:file_name>/', serve_file, name='serve_file'),
-    re_path(r'^media/(?P<file_key>.+)/$', download_file_from_s3, name='serve_file'),
+    re_path(r'^media/(?P<file_key>.+)/$', download_file_from_s3, name='download_file'),
     # path('download/<str:file_key>/',download_file_from_s3, name='download_file'),
 
 ]
