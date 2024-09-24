9bb7da0472cc0f937ac9594778c12d29eab63cfc
ubongpr7 <ubongpr@gmail.com>
2024-09-24 07:02:01 +0100

    hmm

--- mainapps/accounts/models.py
+++ mainapps/accounts/models.py
@@ -75,9 +75,13 @@ class Credit(models.Model):
     last_reset = models.DateTimeField(auto_now_add=True)
     @classmethod
     def deduct_credits(self, amount):
-        current_credits = self.credits  # This fetches the actual value
-        self.credits = current_credits - amount
-        self.save()
+        self.refresh_from_db(fields=['credits'])
+    
+        # Now operate on the actual value of `credits`
+        if self.credits >= amount:
+            self.credits -= amount  # This will now work correctly
+            self.save()
+
 
     def reset_credits(self, monthly_credits):
         if (now() - self.last_reset) >= timedelta(days=30):
