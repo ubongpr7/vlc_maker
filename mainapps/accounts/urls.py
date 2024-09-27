429171aa5f1f72710bec774db28d7dfca0e0edd6
ubongpr7 <ubongpr7@gmail.com>
2024-09-27 03:48:42 +0100

    finishing up assets

--- mainapps/accounts/urls.py
+++ mainapps/accounts/urls.py
@@ -14,7 +14,7 @@ urlpatterns=[
 
     # path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     # path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
