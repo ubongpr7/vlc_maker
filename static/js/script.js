00e9d44a0ac57d5f7ae17f879eba9006e3aae6f8
ubongpr7 <ubongpr@gmail.com>
2024-09-27 16:34:14 +0100

    Fixings

--- static/js/script.js
+++ static/js/script.js
@@ -204,18 +204,18 @@ let colorvalue2 = '#FFFFFF';
 
 
 
-const pfp = document.getElementById('pfp');
-const dropdown = document.getElementById('pfpdropdown');
+// const pfp = document.getElementById('pfp');
+// const dropdown = document.getElementById('pfpdropdown');
 
-    pfp.addEventListener('click', function() {
-        dropdown.classList.toggle('not-present');
-        dropdown.classList.toggle('present');
-    });
+//     pfp.addEventListener('click', function() {
+//         dropdown.classList.toggle('not-present');
+//         dropdown.classList.toggle('present');
+//     });
 
-    document.addEventListener('click', function(event) {
-        if (!dropdown.contains(event.target) && !pfp.contains(event.target)) {
-            dropdown.classList.remove('present');
-            dropdown.classList.add('not-present');
-        }
-    });
+//     document.addEventListener('click', function(event) {
+//         if (!dropdown.contains(event.target) && !pfp.contains(event.target)) {
+//             dropdown.classList.remove('present');
+//             dropdown.classList.add('not-present');
+//         }
+//     });
 
