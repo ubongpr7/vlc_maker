47029339cc19bb9eebb3d8fde68e8533da6392bd
ubongpr7 <ubongpr@gmail.com>
2024-09-29 11:40:47 +0100

    button

--- mainapps/accounts/urls.py
+++ mainapps/accounts/urls.py
@@ -7,15 +7,6 @@ urlpatterns=[
     path('pricing/',embedded_pricing_page,name='embedded_pricing_page'),
     path('confirm-subscription/',subscription_confirm,name='subscription_confirm'),
     path('logout/', logout_view, name='logout'),  
-    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    # path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    # path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 
