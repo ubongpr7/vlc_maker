7b9b7f501976c989fa9e4f88982120e5349d0694
ubongpr7 <ubongpr@gmail.com>
2024-09-29 19:59:07 +0100

    button

--- mainapps/home/views.py
+++ mainapps/home/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+# Create your views here.from django.shortcuts import redirect
+
 def home(request):
-    return render(request,'vlc/frontend/landing.html')
+    if request.user.is_authenticated:
+        return redirect('/text')
+    else:
+        return render(request, 'vlc/frontend/landing.html')
+
 
 
 
