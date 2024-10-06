e162cd1812f90da1c6942d456ba42a38d72309e5
ubongpr7 <ubongpr@gmail.com>
2024-10-06 15:44:37 +0100

    Bg music

--- static/landing/script.js
+++ static/landing/script.js
@@ -24,24 +24,24 @@ setInterval(() => {
   showSlide(currentIndex);
 }, 3000); // Change every 3 seconds
 
-function toggleMenu() {
-  const topbar = document.getElementById("topbar");
-  const hamburger = document.getElementById("hamburger");
+// function toggleMenu() {
+//   const topbar = document.getElementById("topbar");
+//   const hamburger = document.getElementById("hamburger");
 
-  // Check if the topbar is currently hidden (offscreen)
-  const open = topbar.style.top === "0px";
+//   // Check if the topbar is currently hidden (offscreen)
+//   const open = topbar.style.top === "0px";
 
-  if (open) {
-    // If menu is open, close it and change the icon back to hamburger
-    topbar.style.top = "-497px"; // Move topbar offscreen
-    hamburger.src = "{% statc 'assets/icons/Hamburger.svg' %}";
-  } else {
-    // If menu is closed, open it and change the icon to "x"
-    topbar.style.top = "0"; // Slide topbar into view
-    hamburger.src = "{% static 'assets/icons/x.svg' %}";
-  }
-}
+//   if (open) {
+//     // If menu is open, close it and change the icon back to hamburger
+//     topbar.style.top = "-497px"; // Move topbar offscreen
+//     hamburger.src = "{% statc 'assets/icons/Hamburger.svg' %}";
+//   } else {
+//     // If menu is closed, open it and change the icon to "x"
+//     topbar.style.top = "0"; // Slide topbar into view
+//     hamburger.src = "{% static 'assets/icons/x.svg' %}";
+//   }
+// }
 
-hamburger.addEventListener("click", () => {
-  toggleMenu();
-});
+// hamburger.addEventListener("click", () => {
+//   toggleMenu();
+// });
