9988f04676f80a9875ef92d89c2a979255e1897d
Ubong Prosper <ubongpr7@gmail.com>
2024-10-15 22:36:11 +0100

    Email setup

--- mainapps/accounts/emails.py
+++ mainapps/accounts/emails.py
@@ -35,15 +35,25 @@ class EmailThread(threading.Thread):
         if self.email_message.send():
             print('Email sent successfully')
 
-
 def send_html_email(subject, message, from_email, to_email, html_file, context):
+    # Render the HTML content
     html_content = render_to_string(html_file, context)
-    text_content = strip_tags(html_content)
 
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+    # Create the email object with just the HTML content
+    msg = EmailMultiAlternatives(subject, "", from_email, [to_email])  # Empty text content
     msg.attach_alternative(html_content, "text/html")
+    
     print("Sending email...")
     EmailThread(msg).start()
+
+# def send_html_email(subject, message, from_email, to_email, html_file, context):
+#     html_content = render_to_string(html_file, context)
+#     text_content = strip_tags(html_content)
+
+#     msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+#     msg.attach_alternative(html_content, "text/html")
+#     print("Sending email...")
+#     EmailThread(msg).start()
 
 
 def welcome_message(user):
