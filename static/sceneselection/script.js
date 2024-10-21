1dfea0c8e1c0626536a84ec9508865b2f494d6c4
ubongpr7 <ubongpr@gmail.com>
2024-10-21 01:38:21 +0100

    Scene fix

--- static/sceneselection/script.js
+++ static/sceneselection/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         input.addEventListener('change', (event) => {
             const file = event.target.files[0];
             if (file) {
-                uploadTexts[index].textContent = `${file.name.slice(0,10)}`;
+                uploadTexts[index].textContent = `${file.name.slice(0,15)}`;
                 svgs[index].style.position = 'absolute';
                 svgs[index].style.opacity = '0';
                 selected_clips[index].value= ""
