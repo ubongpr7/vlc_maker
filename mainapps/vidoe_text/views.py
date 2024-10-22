6df8739ea1eb6c6d2176f213165fe61e9c2cc94f
ubongpr7 <ubongpr@gmail.com>
2024-10-22 03:56:22 +0100

    Background Music update

--- mainapps/vidoe_text/views.py
+++ mainapps/vidoe_text/views.py
@@ -310,11 +310,13 @@ def process_background_music(request, textfile_id):
 
         
         if n_musics< no_of_mp3:
-            for i, music_file in enumerate(music_files, start=n_musics):
-                if music_files_dict.get(f'bg_music_{i}'):
+            # for i, music_file in enumerate(music_files, start=n_musics):
+            for i, (key, music_file) in enumerate(music_files_dict.items(), start=n_musics):
+                # if music_files_dict.get(f'bg_music_{i}'):
+                if music_file:
                     bg_music=BackgroundMusic(
                             text_file=textfile,
-                            music=music_files_dict.get(f'bg_music_{i}'),
+                            music=music_file,
                             start_time=start_times[i-1],
                             end_time=end_times[i-1],
                             bg_level=bg_levels[f"bg_music_{i}"]
