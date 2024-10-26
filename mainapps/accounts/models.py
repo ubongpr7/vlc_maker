1656b54b48c7400a318b42e4339bd12d7705e504
Ubong Prosper <ubongpr7@gmail.com>
2024-10-26 18:19:25 +0100

    Process video

--- mainapps/accounts/models.py
+++ mainapps/accounts/models.py
@@ -5,8 +5,8 @@ from django.db import models
 from djstripe.models import Product
 from django.utils.timezone import now
 from datetime import timedelta
-
 from django.conf import settings
+
 from datetime import datetime, timedelta
 
 class CustomUserManager(BaseUserManager):
@@ -33,10 +33,10 @@ class User(AbstractUser):
         'djstripe.Subscription', null=True, blank=True, on_delete=models.SET_NULL,
         help_text="The user's Stripe Subscription object, if it exists",
     )
-    customer = models.ForeignKey(
-        'djstripe.Customer', null=True, blank=True, on_delete=models.SET_NULL,
-        help_text="The user's Stripe Customer object, if it exists"
-    )    
+    # customer = models.ForeignKey(
+    #     'djstripe.Customer', null=True, blank=True, on_delete=models.SET_NULL,
+    #     help_text="The user's Stripe Customer object, if it exists"
+    # )    
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
